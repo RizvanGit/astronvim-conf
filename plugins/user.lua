159a622ccb4d47f49b5ca70d7c0896b8dc00659e
Rizvan <imeandmyep2@gmail.com>
2023-06-27 16:20:51 +0300

    updated shortkeys

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,9 @@
 return {
   -- You can also add new plugins here as well:
+  "morhetz/gruvbox",
+  "ThePrimeagen/vim-be-good",
+  "windwp/nvim-ts-autotag",
+  "nvim-treesitter/nvim-treesitter",
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
   -- {
